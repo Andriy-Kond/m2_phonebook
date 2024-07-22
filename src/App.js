07a4d1f9b6b724d4acffe1ae06ad2f2c7afac683
Andrii <andrii.kondratovich@gmail.com>
2024-07-22 17:22:14 +0300

    add: step1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,62 @@
-function App() {
-  return (
-    <div>
-      <input type="text" name="name" required />
-    </div>
-  );
+import React, { Component } from "react";
+import { nanoid } from "nanoid";
+// model.id = nanoid();
+
+class App extends Component {
+  state = {
+    contacts: [],
+    name: "sss",
+  };
+
+  handleContact = e => {
+    this.setState({ name: e.currentTarget.value });
+  };
+
+  addContact = e => {
+    e.preventDefault();
+    const name = e.currentTarget.elements.name.value;
+
+    this.setState(prevState => {
+      return {
+        ...prevState,
+        contacts: [...prevState.contacts, { id: nanoid(), name: name }],
+      };
+    });
+
+    this.clearStateName();
+  };
+
+  clearStateName = () => {
+    this.setState({ name: "" });
+  };
+
+  render() {
+    return (
+      <>
+        <h2>Phonebook</h2>
+        <form onSubmit={this.addContact}>
+          <label>
+            Name
+            <input
+              type="text"
+              name="name"
+              required
+              value={this.state.name}
+              onChange={this.handleContact}
+            />
+          </label>
+          <button type="submit">Add contact</button>
+        </form>
+
+        <h2>Contacts</h2>
+        <ul>
+          {this.state.contacts.map(({ id, name }) => {
+            return <li key={id}>{name}</li>;
+          })}
+        </ul>
+      </>
+    );
+  }
 }
 
 export default App;
