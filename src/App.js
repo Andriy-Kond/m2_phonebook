1373aecdae24183a916ed1960f8be4277e9de8a0
Andrii <andrii.kondratovich@gmail.com>
2024-07-22 17:33:22 +0300

    add: step2

--- src/App.js
+++ src/App.js
@@ -6,20 +6,26 @@ class App extends Component {
   state = {
     contacts: [],
     name: "sss",
+    number: "",
   };
 
   handleContact = e => {
-    this.setState({ name: e.currentTarget.value });
+    const { name } = e.currentTarget;
+    this.setState({ [name]: e.currentTarget.value });
   };
 
   addContact = e => {
     e.preventDefault();
     const name = e.currentTarget.elements.name.value;
+    const number = e.currentTarget.elements.number.value;
 
     this.setState(prevState => {
       return {
         ...prevState,
-        contacts: [...prevState.contacts, { id: nanoid(), name: name }],
+        contacts: [
+          ...prevState.contacts,
+          { id: nanoid(), name: name, number: number },
+        ],
       };
     });
 
@@ -27,10 +33,11 @@ class App extends Component {
   };
 
   clearStateName = () => {
-    this.setState({ name: "" });
+    this.setState({ name: "", number: "" });
   };
 
   render() {
+    const { name, number } = this.state;
     return (
       <>
         <h2>Phonebook</h2>
@@ -41,17 +48,33 @@ class App extends Component {
               type="text"
               name="name"
               required
-              value={this.state.name}
+              value={name}
               onChange={this.handleContact}
             />
+          </label>
+
+          <label>
+            Telephone
+            <input
+              type="tel"
+              name="number"
+              required
+              value={number}
+              onChange={this.handleContact}></input>
           </label>
           <button type="submit">Add contact</button>
         </form>
 
         <h2>Contacts</h2>
         <ul>
-          {this.state.contacts.map(({ id, name }) => {
-            return <li key={id}>{name}</li>;
+          {this.state.contacts.map(({ id, name, number }) => {
+            return (
+              <li key={id}>
+                <p>
+                  {name}: {number}
+                </p>
+              </li>
+            );
           })}
         </ul>
       </>
